bf5a1eb15616ebdc606323e0e272ecf419dbca17
Andrea Dao <andrea.dao@bigcommerce.com>
2024-05-02 11:02:09 -0500

    remove filter_mode

--- .github/actions/retext-style-guide/entrypoint.sh
+++ .github/actions/retext-style-guide/entrypoint.sh
@@ -13,7 +13,6 @@ find ${GITHUB_WORKSPACE} -type f -name '*.mdx' -exec quality-docs {} + \
   | reviewdog -f=remark-lint \
       -name="remark-lint" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
-#      -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
       ${INPUT_REVIEWDOG_FLAGS} \
