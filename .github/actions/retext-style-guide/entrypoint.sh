ea5ee51e0e1349878e810e12cca39783c40d415b
Andrea Dao <andrea.dao@bigcommerce.com>
2024-05-02 12:21:54 -0500

    add in old linter, remove new linter

--- .github/actions/retext-style-guide/entrypoint.sh
+++ .github/actions/retext-style-guide/entrypoint.sh
@@ -13,8 +13,8 @@ find ${GITHUB_WORKSPACE} -type f -name '*.mdx' -exec quality-docs {} + \
   | reviewdog -f=remark-lint \
       -name="remark-lint" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
+      -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
       ${INPUT_REVIEWDOG_FLAGS} \
       -tee \
-      -diff="git diff --unified=0 ${INPUT_BEFORE_COMMIT} ${INPUT_AFTER_COMMIT}"
